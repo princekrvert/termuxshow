aa209bc96b259af952a7365b4cc7d7ecd51d8757
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2019-11-23 16:08:33 +0530

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,109 @@
+#!/data/data/com.termux/files/usr/bin/bash
+figlet -f big prince | lolcat 
+apt update && apt upgade
+apt install git 
+apt-get install cawsay
+pkg install cowsay
+pkg install lolcat
+apt-get imstall lolcat
+apt-get install cmatrix
+apt install python -y
+apt install toilet
+apt update && apt upgade
+read -p "enter your name " name
+toilet " $name " --metal
+ pkg install cmatrix
+ 
+ 
+ echo -e  " \e[35m //////// tools /////////// \e[0m"
+ echo -e "\e[35m ****************************** \e[0m"
+ echo -e "\e[38m ****************************** \e[0m"
+ 
+echo -e "\e[33m ****************************** \e[0m"
+ 
+ echo -e "\e[32m ****************************** \e[0m"
+ echo -e "\e[33m ****************************** \e[0m"
+ echo -e " \e[32m 1. banner \e[0m"
+ echo -e " \e[32m 2. fbi \e[0m"
+ echo -e " \e[32m 3. hacking effect \e[0m"
+ echo -e " \e[32m 4. cowsay \e[0m"
+ echo -e " \e[32m 5.emailbom \e[0m"
+ echo -e " \e[32m 6.gmailbrut \e[0m"
+ echo -e " \e[32m 7.youtube downloder \e[0m"
+  echo -e " \e[32m 8.about \e[0m"
+  echo -e "\e[36m=====================================================\e[0m"
+ read -p " type your choise " too 
+ 
+ case  $too  in
+ 
+ "1")
+ echo -e "\e[32m banner installing \e[0m" 
+ git clone https://github.com/Bhai4You/Termux-Banner
+ cowsay pkg installed " $name "
+ 
+  ;;
+ 
+ "2")
+ echo -e "\e[32m fbi installing \e[0m" 
+git clone https://github.com/xHak9x/fbi.git
+cowsay pkg installed " $name "
+ ;;
+ 
+ "3")
+ 
+ cowsay pkg installing " $name "
+ echo -e "\e[32m installing \e[0m" 
+apt-get install cmatrix
+cmatrix
+ ;;
+
+ "4")
+ echo -e "\e[32m cowsay \e[0m" 
+pkg install cowsay
+cowsay pkg installed " $name "
+ ;;
+ "5" )
+ cowsay pkg insralling
+ git clone https://github.com/princekrvert/gmailbomber.git 
+ ;;
+  "6" )
+ cowsay pkg insralling
+ git clone https://github.com/princekrvert/hackgmail.py-.git
+ ;;
+   "7" )
+ cowsay pkg insralling
+ git clone https://github.com/princekrvert/YouTube_D.git
+ ;;
+ 
+ "8" )
+echo -e "\e[35m ****************************** \e[0m"
+ echo -e "\e[38m ****************************** \e[0m"
+ 
+echo -e "\e[33m ****************************** \e[0m"
+ 
+ echo -e "\e[32m ****************************** \e[0m"
+ echo -e "\e[33m ****************************** \e[0m"
+ echo -e "\e[35m ****************************** \e[0m"
+ echo -e "\e[38m ****************************** \e[0m"
+ 
+echo -e "\e[33m ****************************** \e[0m"
+ 
+ echo -e "\e[32m ****************************** \e[0m"
+ echo -e "\e[33m ****************************** \e[0m"
+ 
+ echo -e "\e[36m author: prince kumar \e[0m"
+ echo -e "\e[35m ****************************** \e[0m"
+ echo -e "\e[38m ****************************** \e[0m"
+ 
+echo -e "\e[33m ****************************** \e[0m"
+ 
+ echo -e "\e[32m ****************************** \e[0m"
+ echo -e "\e[33m ****************************** \e[0m"       
+ echo -e "\e[34m( email:princekrvert@gmail.com) \e[0m"
+  ;;
+ "*")
+ echo -e "\e[32m wrong keyword \e[0m" 
+
+ ;;
+
+ esac
